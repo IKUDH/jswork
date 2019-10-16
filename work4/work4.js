b9b561892c74e30683d5a86c92516071094a2fdd
IKUDH <3224163897@qq.com>
2019-10-16 13:07:55 +0800

    work3

--- work4/work4.js
+++ work4/work4.js
@@ -18,5 +18,11 @@ for(let i=1;i<=level;++i){
 }
 let str = '<table border="1">'
 for(let i=1;i<10;++i){
-    str +i='<tr>'
+    str +='<tr>'
+    for(var j=1;j<=i;++j){
+        str +='<td>' + j + '*' + i + '=' + (j*i) + '</td>'
+    }
+    str += '</td>'
 }
+str += '</table>'
+document.getElementById('table').innerHTML = str
