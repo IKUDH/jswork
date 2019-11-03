6a3168feab6609958edcee7da6d384290a7c1ae3
IKUDH <3224163897@qq.com>
2019-11-03 19:40:11 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,13 +1,13 @@
 function goldbach() {
-    let value= document.getElementById('odd').value
-    if(value%2!==0){
+    let aaa= document.getElementById('odd').value
+    if(aaa%2!==0){
         alert("请输入偶数")
         return false
     }
     let goldbach= document.getElementById('goldbach')
     let arr =[]
     let a = 0;
-    for(var i=2;i<=value;i++){
+    for(var i=2;i<=aaa;i++){
         a = 0
         for(var j=2;j<i;j++){
             if(i%j==0){
@@ -21,9 +21,9 @@ function goldbach() {
     let str = ''
     for(let i=0;i<(arr.length)/2;i++){
        for(let j=0;j<arr.length;j++){
-           if((arr[i]+arr[j])===Number(value)){
-               str += '<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'
-              console.log(value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和")
+           if((arr[i]+arr[j])===Number(aaa)){
+              str += '<div>'+aaa+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'
+              console.log(aaa+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和")
            }
        }
     }
