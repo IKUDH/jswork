3601d187d8ee33bcab40b0976eb4c398816a85e3
IKUDH <55221415+IKUDH@users.noreply.github.com>
2019-12-11 23:47:57 +0800

    Update index2.js

--- work14/index2.js
+++ work14/index2.js
@@ -1,16 +1,16 @@
 let time = new Date()
 let fz = miao = 0;
-fz = 60 - time.getMinutes()
-miao = 60 - time.getSeconds()
+fz = 59 - time.getMinutes()
+miao = 59 - time.getSeconds()
 let id =setInterval(seckill,1000)
 function seckill() {
     miao--
     if(miao== -1){
-        miao= 60
+        miao= 59
         fz--
     } 
     if(fz == -1){
-        fz = 60
+        fz = 59
     } 
     document.getElementById('fz').innerHTML = fz +'分'
     document.getElementById('miao').innerHTML = miao +'秒'
